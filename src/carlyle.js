7cdcbcd7b785e98ce9c9b81a0755fb54952a700b
Joseph Pearson <joseph@inventivelabs.com.au>
2010-03-03 20:58:45 +1100

    Compatibility hacks belong in their own source file, now.

--- src/carlyle.js
+++ src/carlyle.js
@@ -1,5 +1,6 @@
 Carlyle = {};
 
+//= require <compat>
 //= require <reader>
 //= require <book>
 //= require <place>
@@ -8,58 +9,3 @@ Carlyle = {};
 
 Carlyle.Flippers = {};
 Carlyle.Controls = {};
-
-// A little console stub if not initialized in a console-equipped browser.
-if (typeof window.console == "undefined") {
-  window.console = {
-    messages: [],
-    log: function (msg) {
-      this.messages.push(msg);
-    }
-  }
-}
-
-
-Carlyle.addListener = function (elem, evtType, fn, useCapture) {
-  if (elem.addEventListener) {
-    return elem.addEventListener(evtType, fn, useCapture || false);
-  } else if (elem.attachEvent) {
-    return elem.attachEvent('on'+evtType, fn);
-  }
-}
-
-
-Carlyle.removeListener = function (elem, evtType, fn, useCapture) {
-  if (elem.removeEventListener) {
-    return elem.removeEventListener(evtType, fn, useCapture || false);
-  } else if (elem.detachEvent) {
-    return elem.detachEvent('on'+evtType, fn);
-  }
-}
-
-
-// IE6 test code
-//
-if (!Array.prototype.indexOf)
-{
-  Array.prototype.indexOf = function(elt /*, from*/)
-  {
-    var len = this.length >>> 0;
-
-    var from = Number(arguments[1]) || 0;
-    from = (from < 0)
-         ? Math.ceil(from)
-         : Math.floor(from);
-    if (from < 0)
-      from += len;
-
-    for (; from < len; from++)
-    {
-      if (from in this &&
-          this[from] === elt)
-        return from;
-    }
-    return -1;
-  };
-}
-
---
+++ src/compat.js
@@ -0,0 +1,57 @@
+/* Standardized event registration - coheres the W3C and MS event models. */
+
+Carlyle.addListener = function (elem, evtType, fn, useCapture) {
+  if (elem.addEventListener) {
+    return elem.addEventListener(evtType, fn, useCapture || false);
+  } else if (elem.attachEvent) {
+    return elem.attachEvent('on'+evtType, fn);
+  }
+}
+
+
+Carlyle.removeListener = function (elem, evtType, fn, useCapture) {
+  if (elem.removeEventListener) {
+    return elem.removeEventListener(evtType, fn, useCapture || false);
+  } else if (elem.detachEvent) {
+    return elem.detachEvent('on'+evtType, fn);
+  }
+}
+
+
+if (typeof(CARLYLE_NO_COMPAT) == 'undefined') {
+
+  // A little console stub if not initialized in a console-equipped browser.
+  if (typeof window.console == "undefined") {
+    window.console = {
+      messages: [],
+      log: function (msg) {
+        this.messages.push(msg);
+      }
+    }
+  }
+
+
+  // indexOf code for IE - ripped from the Mozilla docs.
+  //
+  if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function(elt /*, from*/) {
+      var len = this.length >>> 0;
+
+      var from = Number(arguments[1]) || 0;
+      from = (from < 0)
+        ? Math.ceil(from)
+        : Math.floor(from);
+      if (from < 0) {
+        from += len;
+      }
+
+      for (; from < len; from++) {
+        if (from in this && this[from] === elt) {
+          return from;
+        }
+      }
+      return -1;
+    };
+  }
+
+}
