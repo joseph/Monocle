7aa2bda99127f71b2a345aa01e893b22380b2d8b
Joseph Pearson <joseph@inventivelabs.com.au>
2010-03-30 10:22:14 +1100

    Monocle.Styles declared in monocle.js; styles.js not always required.

--- src/monocle.js
+++ src/monocle.js
@@ -2,6 +2,21 @@ Monocle = {
   VERSION: "1.0.0"
 };
 
+Monocle.Styles = {
+  ruleText: function(rule) {
+    if (typeof rule == "string") {
+      rule = this[rule];
+    }
+    if (!rule) { return ""; }
+
+    var parts = [];
+    for (var declaration in rule) {
+      parts.push(declaration + ": " + rule[declaration] + ";")
+    }
+    return parts.join(" ");
+  }
+}
+
 //= require <compat>
 //= require <reader>
 //= require <book>
