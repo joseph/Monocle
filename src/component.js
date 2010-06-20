f8b87f5c86122703f74b4c1ab863c6ac643cff03
Joseph Pearson <joseph@inventivelabs.com.au>
2010-06-20 12:16:41 +1000

    Fixing clamped stylesheet overrides (newlines).

--- src/component.js
+++ src/component.js
@@ -311,9 +311,11 @@ Monocle.Component = function (book, id, index, chapters, source) {
 
   function clampCSS(doc) {
     // TODO: move to somewhere it can be configured...
-    var rules = "body * { float: none !important; clear: none !important; text-align: left; }" +
-      "p { margin-left: 0 !important; margin-right: 0 !important; }" +
-      "table, img { max-width: 100% !important; max-height: 90% !important; }";
+    var rules = [
+      "body * { float: none !important; clear: none !important; text-align: left !important; }",
+      "p { margin-left: 0 !important; margin-right: 0 !important; }",
+      "table, img { max-width: 100% !important; max-height: 90% !important; }"
+    ].join("\n")
 
     var styleTag = document.createElement('style');
     styleTag.type = 'text/css';
