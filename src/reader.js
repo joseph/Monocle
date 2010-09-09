85c396314d65f924cbb184f58af8e61f8b623cf0
Joseph Pearson <joseph@inventivelabs.com.au>
2010-09-09 19:29:42 +1000

    Add newly created iframes to touchmonitor in Reader.

--- src/reader.js
+++ src/reader.js
@@ -164,6 +164,10 @@ Monocle.Reader = function (node, bookData, options, onLoadCallback) {
       page.m.sheafDiv.appendChild(page.m.activeFrame);
       p.flipper.addPage(page);
       p.divs.container.appendChild(page);
+      // BROWSERHACK: hook up the iframe to the touchmonitor if it exists.
+      if (Monocle.Browser.has.iframeTouchBug && Monocle.Events.tMonitor) {
+        Monocle.Events.tMonitor.listenOnIframe(page.m.activeFrame);
+      }
     }
     p.divs.overlay = document.createElement('div');
     p.divs.box.appendChild(p.divs.overlay);
