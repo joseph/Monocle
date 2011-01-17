1bc08ba3a0a7b6d62f2e9d38dcc9ff778811e886
Joseph Pearson <joseph@inventivelabs.com.au>
2011-01-17 18:17:29 +1100

    Stencil cutouts update position after resize/stylesheetchange.

--- src/controls/stencil.js
+++ src/controls/stencil.js
@@ -13,96 +13,146 @@ Monocle.Controls.Stencil = function (reader) {
 
   function createControlElements(holder) {
     p.container = holder.dom.make('div', k.CLS.container);
-    p.reader.listen('monocle:turn', shift);
-    //p.reader.listen('monocle:stylesheetchange', update);
-    //p.reader.listen('monocle:resize', update);
+    p.reader.listen('monocle:turn', draw);
+    p.reader.listen('monocle:stylesheetchange', update);
+    p.reader.listen('monocle:resize', update);
     return p.container;
+  }
+
+
+  function update() {
+    var pageDiv = p.reader.visiblePages()[0];
+    var cmptId = pageComponentId(pageDiv);
+    p.components[cmptId] = null; // Reset pre-calculated rectangles.
+    getRects(pageDiv);
+    draw();
   }
 
 
   // After a page turn, aligns cutouts to pages. If component changed or
   // resized, calls update.
-  function shift(evt) {
+  function draw() {
     var pageDiv = p.reader.visiblePages()[0];
     var cmptId = pageComponentId(pageDiv);
-    cmpt = pageDiv.m.activeFrame.parentNode;
-    p.container.dom.setStyles({
-      top: cmpt.offsetTop + "px",
-      left: cmpt.offsetLeft + "px"
-    });
-    //if (cmptId != p.activeComponent) {
-      update(pageDiv);
-    //}
+    if (cmptId != p.activeComponent) {
+      getRects(pageDiv);
+    }
+
+    // Position the container.
+    alignToComponent(pageDiv);
+
+    // Layout the cutouts.
+    var placed = 0;
+    var rects = p.components[cmptId];
+    if (rects && rects.length) {
+      placed = layoutRects(pageDiv, rects);
+    }
+
+    // Hide remaining rects.
+    while (placed < p.cutouts.length) {
+      hideCutout(placed);
+      placed += 1;
+    }
+  }
+
+
+  function getRects(pageDiv) {
+    var cmptId = pageComponentId(pageDiv);
+    p.activeComponent = cmptId;
+    var doc = pageDiv.m.activeFrame.contentDocument;
+    var offset = getOffset(pageDiv);
+    var calcRects = false;
+    if (!p.components[cmptId]) {
+      p.components[cmptId] = []
+      calcRects = true;
+    }
+
+    var iElems = doc.getElementsByTagName('a');
+    for (var i = 0; i < iElems.length; ++i) {
+      var href = iElems[i].getAttribute('href');
+      if (href) {
+        if (!iElems[i].processed) {
+          fixLink(iElems[i], href);
+        }
+
+        if (calcRects) {
+          var r = iElems[i].getClientRects();
+          for (var j = 0; j < r.length; j++) {
+            p.components[cmptId].push({
+              href: href,
+              left: Math.ceil(r[j].left + offset.l),
+              top: Math.ceil(r[j].top),
+              width: Math.floor(r[j].width),
+              height: Math.floor(r[j].height)
+            });
+          }
+        }
+      }
+    }
+
+    return p.components[cmptId];
+  }
+
+
+  function getOffset(pageDiv) {
+    var place = p.reader.getPlace();
+    var pages = place.pageNumber() - 1;
+    var result = { w: pageDiv.m.dimensions.properties.measurements.width }
+    result.l = result.w * pages;
+    return result;
   }
 
 
   // Creates the necessary cutouts for the active component on the given page,
   // or updates them if they already exist.
-  function update(pageDiv) {
-    var cmptId = pageComponentId(pageDiv);
-    p.activeComponent = cmptId;
-    var doc = pageDiv.m.activeFrame.contentDocument;
-    var rects = p.components[cmptId];
-    var i;
-    var href;
-    if (!rects) {
-      var rects = p.components[cmptId] = [];
-      var iElems = doc.getElementsByTagName('a');
-      for (i = 0; i < iElems.length; ++i) {
-        if (href = iElems[i].getAttribute('href')) {
-          var r = iElems[i].getClientRects();
-          for (var j = 0; j < r.length; j++) {
-            r[j].href = href;
-            rects.push(r[j]);
-          }
-        }
+  function layoutRects(pageDiv, rects) {
+    var offset = getOffset(pageDiv);
+    var visRects = [];
+    for (var i = 0; i < rects.length; ++i) {
+      if (rectVisible(rects[i], offset.l, offset.l + offset.w)) {
+        visRects.push(rects[i]);
       }
     }
 
-    if (rects.length) {
-      var place = p.reader.getPlace();
-      var pages = place.pageNumber() - 1;
-      var w = pageDiv.m.dimensions.properties.measurements.width;
-      var l = w * pages;
-      var visRects = [];
-      for (i = 0; i < rects.length; ++i) {
-        if (rectVisible(rects[i], l, l+w)) {
-          visRects.push(rects[i]);
-        }
+    // Update location of visible rectangles - creating as required.
+    for (i = 0; i < visRects.length; ++i) {
+      if (!p.cutouts[i]) {
+        p.cutouts[i] = createCutout();
       }
-
-      // Update location of visible rectangles - creating as required.
-      for (i = 0; i < visRects.length; ++i) {
-        if (!p.cutouts[i]) {
-          p.cutouts[i] = p.container.dom.append('a', k.CLS.cutout);
-          p.cutouts[i].setAttribute('target', '_blank');
-        }
-        var link = p.cutouts[i];
-        link.dom.setStyles({
-          display: 'block',
-          left: (visRects[i].left - l)+"px",
-          top: visRects[i].top+"px",
-          width: visRects[i].width+"px",
-          height: visRects[i].height+"px"
-        });
-        Monocle.Events.deafen(link, 'click', cutoutClick);
-        href = link.href = visRects[i].href;
-        if (!k.REGEXES.protocolAndHost.test(href)) {
-          if (k.REGEXES.onlyAnchorInHref.test(href)) {
-            link.href = place.componentId() + href;
-          }
-          Monocle.Events.listen(link, 'click', cutoutClick);
-        }
-      }
-    } else {
-      i = 0;
+      var link = p.cutouts[i];
+      link.dom.setStyles({
+        display: 'block',
+        left: (visRects[i].left - offset.l)+"px",
+        top: visRects[i].top+"px",
+        width: visRects[i].width+"px",
+        height: visRects[i].height+"px"
+      });
+      fixLink(link, visRects[i].href);
     }
 
-    // Hide remaining rects.
-    while (i < p.cutouts.length) {
-      p.cutouts[i].dom.setStyles({ display: 'none' });
-      i += 1;
+    return i;
+  }
+
+
+  function fixLink(link, href) {
+    Monocle.Events.deafen(link, 'click', cutoutClick);
+    link.href = href;
+    link.setAttribute('target', '_blank');
+    if (!k.REGEXES.protocolAndHost.test(href)) {
+      if (k.REGEXES.onlyAnchorInHref.test(href)) {
+        var pageDiv = p.reader.visiblePages()[0];
+        var cmptId = pageComponentId(pageDiv);
+        link.href = cmptId + href;
+      }
+      Monocle.Events.listen(link, 'click', cutoutClick);
     }
+    link.processed = true;
+  }
+
+
+  function createCutout() {
+    var cutout =  p.container.dom.append('a', k.CLS.cutout);
+    return cutout;
   }
 
 
@@ -111,8 +161,22 @@ Monocle.Controls.Stencil = function (reader) {
   }
 
 
-  function rectVisible(rect, left, right) {
-    return Math.ceil(rect.left) >= left && Math.floor(rect.left) < right;
+  function alignToComponent(pageDiv) {
+    cmpt = pageDiv.m.activeFrame.parentNode;
+    p.container.dom.setStyles({
+      top: cmpt.offsetTop + "px",
+      left: cmpt.offsetLeft + "px"
+    });
+  }
+
+
+  function hideCutout(index) {
+    p.cutouts[index].dom.setStyles({ display: 'none' });
+  }
+
+
+  function rectVisible(rect, l, r) {
+    return rect.left >= l && rect.left < r;
   }
 
 
@@ -130,7 +194,7 @@ Monocle.Controls.Stencil = function (reader) {
 
 
   API.createControlElements = createControlElements;
-  API.shift = shift;
+  API.draw = draw;
   API.update = update;
 
   return API;
