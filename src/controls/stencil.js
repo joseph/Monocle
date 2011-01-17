6c5f9f46b1080b79f4b95a99a54ab5c3292929eb
Joseph Pearson <joseph@inventivelabs.com.au>
2011-01-17 16:17:49 +1100

    Functional (but messy) stencil control.

--- src/controls/stencil.js
+++ src/controls/stencil.js
@@ -11,17 +11,11 @@ Monocle.Controls.Stencil = function (reader) {
   }
 
 
-  function initialize() {
-  }
-
-
   function createControlElements(holder) {
     p.container = holder.dom.make('div', k.CLS.container);
     p.reader.listen('monocle:turn', shift);
-    p.container.dom.setStyles({
-      top: "15px",
-      left: "15px"
-    });
+    //p.reader.listen('monocle:stylesheetchange', update);
+    //p.reader.listen('monocle:resize', update);
     return p.container;
   }
 
@@ -31,6 +25,11 @@ Monocle.Controls.Stencil = function (reader) {
   function shift(evt) {
     var pageDiv = p.reader.visiblePages()[0];
     var cmptId = pageComponentId(pageDiv);
+    cmpt = pageDiv.m.activeFrame.parentNode;
+    p.container.dom.setStyles({
+      top: cmpt.offsetTop + "px",
+      left: cmpt.offsetLeft + "px"
+    });
     //if (cmptId != p.activeComponent) {
       update(pageDiv);
     //}
@@ -45,53 +44,58 @@ Monocle.Controls.Stencil = function (reader) {
     var doc = pageDiv.m.activeFrame.contentDocument;
     var rects = p.components[cmptId];
     var i;
+    var href;
     if (!rects) {
       var rects = p.components[cmptId] = [];
-      var iElems = doc.getElementsByTagName('A');
+      var iElems = doc.getElementsByTagName('a');
       for (i = 0; i < iElems.length; ++i) {
-        var r = iElems[i].getClientRects();
-        for (var j = 0; j < r.length; j++) {
-          r[j].href = iElems[i].getAttribute('href');
-          rects.push(r[j]);
+        if (href = iElems[i].getAttribute('href')) {
+          var r = iElems[i].getClientRects();
+          for (var j = 0; j < r.length; j++) {
+            r[j].href = href;
+            rects.push(r[j]);
+          }
         }
       }
     }
 
-    if (!rects.length) { return; }
-
-    var place = p.reader.getPlace();
-    var pages = place.pageNumber() - 1;
-    var w = pageDiv.m.dimensions.properties.measurements.width;
-    var l = w * pages;
-    var visRects = [];
-    for (i = 0; i < rects.length; ++i) {
-      if (rectVisible(rects[i], l, l+w)) {
-        visRects.push(rects[i]);
-      }
-    }
-
-    // Update location of visible rectangles - creating as required.
-    for (i = 0; i < visRects.length; ++i) {
-      if (!p.cutouts[i]) {
-        p.cutouts[i] = p.container.dom.append('a', k.CLS.cutout);
-        p.cutouts[i].setAttribute('target', '_blank');
-      }
-      var link = p.cutouts[i];
-      link.dom.setStyles({
-        display: 'block',
-        left: (visRects[i].left - l)+"px",
-        top: visRects[i].top+"px",
-        width: visRects[i].width+"px",
-        height: visRects[i].height+"px"
-      });
-      Monocle.Events.deafen(link, 'click', cutoutClick);
-      var href = link.href = visRects[i].href;
-      if (!k.REGEXES.protocolAndHost.test(href)) {
-        if (k.REGEXES.onlyAnchorInHref.test(href)) {
-          link.href = place.componentId() + href;
+    if (rects.length) {
+      var place = p.reader.getPlace();
+      var pages = place.pageNumber() - 1;
+      var w = pageDiv.m.dimensions.properties.measurements.width;
+      var l = w * pages;
+      var visRects = [];
+      for (i = 0; i < rects.length; ++i) {
+        if (rectVisible(rects[i], l, l+w)) {
+          visRects.push(rects[i]);
         }
-        Monocle.Events.listen(link, 'click', cutoutClick);
       }
+
+      // Update location of visible rectangles - creating as required.
+      for (i = 0; i < visRects.length; ++i) {
+        if (!p.cutouts[i]) {
+          p.cutouts[i] = p.container.dom.append('a', k.CLS.cutout);
+          p.cutouts[i].setAttribute('target', '_blank');
+        }
+        var link = p.cutouts[i];
+        link.dom.setStyles({
+          display: 'block',
+          left: (visRects[i].left - l)+"px",
+          top: visRects[i].top+"px",
+          width: visRects[i].width+"px",
+          height: visRects[i].height+"px"
+        });
+        Monocle.Events.deafen(link, 'click', cutoutClick);
+        href = link.href = visRects[i].href;
+        if (!k.REGEXES.protocolAndHost.test(href)) {
+          if (k.REGEXES.onlyAnchorInHref.test(href)) {
+            link.href = place.componentId() + href;
+          }
+          Monocle.Events.listen(link, 'click', cutoutClick);
+        }
+      }
+    } else {
+      i = 0;
     }
 
     // Hide remaining rects.
@@ -108,7 +112,7 @@ Monocle.Controls.Stencil = function (reader) {
 
 
   function rectVisible(rect, left, right) {
-    return rect.left >= left && rect.left < right;
+    return Math.ceil(rect.left) >= left && Math.floor(rect.left) < right;
   }
 
 
@@ -128,8 +132,6 @@ Monocle.Controls.Stencil = function (reader) {
   API.createControlElements = createControlElements;
   API.shift = shift;
   API.update = update;
-
-  initialize();
 
   return API;
 }
