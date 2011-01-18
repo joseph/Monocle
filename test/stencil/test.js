afc21390190239c98741fcf625a0e91bf38cee29
Joseph Pearson <joseph@inventivelabs.com.au>
2011-01-18 15:57:04 +1100

    Updated the Links test to use the stencil.

---
+++ test/stencil/test.js
@@ -0,0 +1,65 @@
+(function () {
+  var bookData = {
+    getComponents: function () {
+      return ['content/0.html', 'content/1/1.html', 'content/2/2.html'];
+    },
+    getComponent: function (cmptId) {
+      return { url: cmptId }
+    },
+    getContents: function () {
+      return [
+        {
+          title: "0",
+          src: "content/0.html"
+        },
+        {
+          title: "1",
+          src: "content/1/1.html"
+        },
+        {
+          title: "2",
+          src: "content/2/2.html#heading"
+        },
+        {
+          title: "3 (does not exist)",
+          src: "content/3/3.html"
+        }
+      ];
+    },
+    getMetaData: function(key) {
+      return { title: "Lorem Ipsum", creator: "Mr Monocle" }[key];
+    }
+  }
+
+  var rdrOptions = {
+    panels: Monocle.Panels.IMode
+  }
+
+  // Initialize the reader element.
+  Monocle.Events.listen(
+    window,
+    'load',
+    function () {
+      Monocle.Reader('reader', bookData, rdrOptions, function (rdr) {
+        window.reader = rdr;
+        alertIfComponentNotFound(rdr);
+        createStencil(rdr);
+      });
+    }
+  );
+
+
+  function alertIfComponentNotFound(rdr) {
+    rdr.listen('monocle:notfound', function () { alert('Not found.') });
+  }
+
+
+  function createStencil(rdr) {
+    var stencil = new Monocle.Controls.Stencil(rdr);
+    rdr.addControl(stencil);
+    // This line is useful mostly for testing:
+    // it highlights the stencil's link cutouts.
+    stencil.toggleHighlights();
+  }
+
+})();
