0dbafea9194103b95d33fd44178514a1d0210ede
Joseph Pearson <joseph@inventivelabs.com.au>
2011-09-26 11:15:22 +1000

    Added place#pagesInComponent.
    This makes it easier to display a page number control like "Page 13 of
    73" or "60 pages left in this chapter."

--- src/core/place.js
+++ src/core/place.js
@@ -61,6 +61,11 @@ Monocle.Place = function () {
   }
 
 
+  function pagesInComponent() {
+    return p.component.lastPageNumber();
+  }
+
+
   function chapterInfo() {
     if (p.chapter) {
       return p.chapter;
@@ -133,6 +138,7 @@ Monocle.Place = function () {
   API.percentageThrough = percentAtBottomOfPage;
   API.pageAtPercentageThrough = pageAtPercentageThrough;
   API.pageNumber = pageNumber;
+  API.pagesInComponent = pagesInComponent;
   API.chapterInfo = chapterInfo;
   API.chapterTitle = chapterTitle;
   API.chapterSrc = chapterSrc;
