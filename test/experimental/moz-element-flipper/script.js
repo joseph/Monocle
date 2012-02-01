7fb433f7a30399905979b91be97280a6733da1b2
Joseph Pearson <joseph@inventivelabs.com.au>
2012-02-01 12:37:00 +1100

    Go backwards as well as forwards with element flipper.

--- test/experimental/moz-element-flipper/script.js
+++ test/experimental/moz-element-flipper/script.js
@@ -1,19 +1,71 @@
-function jump() {
-  document.body.className = 'pageTurn';
-  setTimeout(nextPage, 420);
-}
+(function () {
 
-function nextPage() {
-  document.body.className = '';
-  window.pos = (window.pos || 0) + 340;
-  setBP(document.getElementById('imp1'), 0)
-  setBP(document.getElementById('imp2'), 1)
-  setBP(document.getElementById('imp3'), 2)
-  setBP(document.getElementById('imp4'), 3)
-}
+  var pos = 0;
+  var parts = {};
 
-function setBP(elem, n) {
-  var x = n*160+window.pos;
-  if (n >= 2) { x += 20; }
-  elem.style.backgroundPosition = "-"+x+"px 0px";
-}
+
+  function init() {
+    parts.mode = document.getElementById('mode');
+    parts.frame = document.getElementById('frame');
+    parts.imp1 = document.getElementById('imp1');
+    parts.imp2 = document.getElementById('imp2');
+    parts.imp3 = document.getElementById('imp3');
+    parts.imp4 = document.getElementById('imp4');
+
+    parts.imp1.addEventListener('click', reverseJump, false);
+    parts.imp2.addEventListener('click', jump, false);
+    impBPs(0,1,2,3);
+  }
+
+
+  function jump() {
+    parts.mode.className = 'forwardTurn';
+    parts.frame.className = 'pageTurn';
+    setTimeout(nextPage, 620);
+  }
+
+
+  function reverseJump() {
+    parts.mode.className = 'reverseTurn';
+    pos -= 340;
+    impBPs(0,2,1,3);
+    setTimeout(function () {
+      parts.frame.className = 'pageTurn';
+      setTimeout(prevPage, 620);
+    }, 50);
+  }
+
+
+  function nextPage() {
+    parts.frame.className = '';
+    pos += 340;
+    impBPs(0,1,2,3);
+  }
+
+
+  function prevPage() {
+    parts.mode.className = 'forwardTurn';
+    parts.frame.className = '';
+    impBPs(0,1,2,3);
+  }
+
+
+  function impBPs(a,b,c,d) {
+    setBP(parts.imp1, a)
+    setBP(parts.imp2, b)
+    setBP(parts.imp3, c)
+    setBP(parts.imp4, d)
+  }
+
+
+  function setBP(elem, n) {
+    var x = n*160+pos;
+    if (n >= 2) { x += 20; }
+    x = 0 - x;
+    elem.style.backgroundPosition = x+"px 0px";
+  }
+
+
+  window.addEventListener('load', init, false);
+
+})();
