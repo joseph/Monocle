83cf064eda152074855fb5a1ebb04580822ac86a
Joseph Pearson <joseph@inventivelabs.com.au>
2012-02-08 18:30:44 +1100

    A copyright/license statement in the code is always helpful.

--- src/core/monocle.js
+++ src/core/monocle.js
@@ -1,3 +1,10 @@
+/*!
+ * Monocle - A silky, tactile browser-based ebook JavaScript library.
+ *
+ * Copyright 2012, Joseph Pearson
+ * Licensed under the MIT license.
+ */
+
 Monocle = {
   VERSION: "2.3.0"
 };
